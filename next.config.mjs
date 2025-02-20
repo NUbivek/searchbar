23ce54d677a484f07693fbdc66065ee034265b39
Bivek Adhikari <bivek@u.northwestern.edu>
2025-02-20 09:02:38 -0800

    Fix build configuration for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: '.next',
-  trailingSlash: true
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
