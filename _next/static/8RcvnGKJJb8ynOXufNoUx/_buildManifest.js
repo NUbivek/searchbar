dfb4d499e040c3e582186008a7e12b2a739e4109
NUbivek <NUbivek@users.noreply.github.com>
2025-02-23 02:54:23 +0000

    deploy: 6a9db16a603e538e7aacdcb62e35b47c31331ae8

--- _next/static/8RcvnGKJJb8ynOXufNoUx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-dae04073cc484702.js"],"/_error":["static/chunks/pages/_error-feb92383c0cdd483.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
