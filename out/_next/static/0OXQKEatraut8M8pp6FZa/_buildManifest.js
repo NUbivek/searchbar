19814b17f2443283c1976f6d7e5f04f1a5338e13
Bivek Adhikari <bivek@u.northwestern.edu>
2025-02-22 18:17:17 -0800

    Deploy: Restore UI to exact design from 04f826f

---
+++ out/_next/static/0OXQKEatraut8M8pp6FZa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-dae04073cc484702.js"],"/_error":["static/chunks/pages/_error-feb92383c0cdd483.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
