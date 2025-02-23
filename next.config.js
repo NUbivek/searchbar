1762f4b31edc83b8c10bf7b50ab049bc5dff0255
Bivek Adhikari <bivek@u.northwestern.edu>
2025-02-22 19:03:19 -0800

    Fix static export configuration

--- next.config.js
+++ next.config.js
@@ -4,20 +4,6 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With,Content-Type' },
-          { key: 'Content-Type', value: 'application/json' }
-        ]
-      }
-    ];
   }
 };
 
